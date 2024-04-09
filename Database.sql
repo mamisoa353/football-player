5034cab9edfaace27712b6dc35d77095e8fea8d0
mamisoa353 <69222069+mamisoa353@users.noreply.github.com>
2024-04-09 11:57:32 +0300

    update affichage nationalite

--- Database.sql
+++ Database.sql
@@ -2,4 +2,21 @@ CREATE TABLE Nationalite(
     Id serial PRIMARY KEY,
     Designation VARCHAR,
     Drapeau VARCHAR
-)
+);
+CREATE TABLE Ligue(
+    Id serial PRIMARY KEY,
+    Designation VARCHAR,
+    IdNationalite int
+);
+INSERT INTO Ligue(Designation, IdNationalite)
+values('Premier Ligue', 3),
+    ('LaLiga', 4),
+    ('Ligue 1', 2),
+    ('MLS', 5);
+CREATE TABLE ClubTeam(
+    Id serial PRIMARY KEY,
+    Nom VARCHAR unique,
+    Profil VARCHAR,
+    Code VARCHAR(3),
+    IdLigue int
+);
--- resources/views/nationalite/liste.blade.php
+++ resources/views/nationalite/liste.blade.php
@@ -4,97 +4,103 @@
 
         <div class="card-body"><a href='add'> <button class="btn btn-success">Ajouter nouveau</button></a>
 
-            <h3>Liste Nationalite</h3>
-            <div class="table-responsive pt-3">
-                <table class="table table-striped" border="1">
-                    <tr>
-                        <th>Designation</th>
-                        <th>Drapeau</th>
-
-                    </tr>
+            <div class="site-section bg-dark">
+                <div class="container">
 
 
-
-                    @foreach ($liste as $row)
-                        <tr>
-                            <td><?php echo $row->designation; ?></td>
-                            <td><?php echo $row->drapeau; ?></td>
-                            <td>
-                                <button type="button" class="btn btn-success mb-2" data-bs-toggle="modal"
-                                    data-bs-target="#basicModal<?php echo $row->id; ?>">Modif</button>
-
-                            </td>
-                            <div class="modal fade" id="basicModal<?php echo $row->id; ?>">
-                                <div class="modal-dialog  modal-md" role="document">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title">Modification</h5>
-                                        </div>
-                                        <div class="modal-body">
-
-
-
-                                            <form action="action_update" method="GET"class="row g-3">
-                                                <div class="col-md-6">
-                                                    <label for="inputEmail1" class="form-label">Designation</label>
-                                                    <input type="text" class="form-control"
-                                                        placeholder='designation'name='designation'
-                                                        value=<?= $row->designation ?>>
-                                                </div>
-                                                <div class="col-md-6">
-                                                    <label for="inputEmail2" class="form-label">Drapeau</label>
-                                                    <input type="text" class="form-control"
-                                                        placeholder='drapeau'name='drapeau' value=<?= $row->drapeau ?>>
-                                                </div>
-                                                <input type="hidden" class="form-control" name='id'
-                                                    value='<?= $row->id ?>' />
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-danger light"
-                                                data-bs-dismiss="modal">Close</button>
-                                            <button type="submit" class="btn btn-primary">Save changes</button>
-                                        </div>
-                                        </form>
-
-                                    </div>
-                                    </td>
-                                    <td>
-                                        <button type="button" class="btn btn-danger mb-2" data-bs-toggle="modal"
-                                            data-bs-target="#bbasicModal<?php echo $row->id; ?>">Supprimer</button>
-
-                                    </td>
-                                    <div class="modal fade" id="bbasicModal<?php echo $row->id; ?>">
-                                        <div class="modal-dialog  modal-md" role="document">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <h5 class="modal-title">Suppression</h5>
-                                                </div>
-                                                <div class="modal-body">
-
-
-
-                                                    <form action="supprimer" method="GET"class="row g-3">
-                                                        <p style='font-size:28px'>Confirmation de la suppression!</p> <input
-                                                            type="hidden" class="form-control" name='id'
-                                                            value='<?= $row->id ?>' />
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-primary light"
-                                                        data-bs-dismiss="modal">Close</button>
-                                                    <button type="submit" class="btn btn-danger">Supprimer</button>
-                                                </div>
-                                                </form>
-
-                                            </div>
+                    <div class="row" id="listeClubs">
+                        <div class="col-lg-12">
+                            <div class="col-6 title-section">
+                                <h3 class="heading">Liste des nationalité</h3>
+                            </div>
+                            <div class="widget-next-match">
+                                <table class="table table-striped" border="1">
+                                    <tr>
+                                        <th>Designation</th>
+                                        <th>Drapeau</th>
+                                    </tr>
+                                    @foreach ($liste as $row)
+                                        <tr>
+                                            <td><?php echo $row->designation; ?></td>
+                                            <td> <img
+                                                    src="{{ asset('/assets/logo/Nationalite/'.$row->drapeau.'.svg') }}"
+                                                    class="rounded-circle mr-3" width="15" height="15">
+                                                {{-- <?php echo $row->drapeau; ?>  --}}
                                             </td>
-                        </tr>
-                    @endforeach
-                </table>
-
+                                            <td>
+                                                <button type="button" class="btn btn-success mb-2" data-bs-toggle="modal"
+                                                    data-bs-target="#basicModal<?php echo $row->id; ?>">Modif</button>
+                                            </td>
+                                            <div class="modal fade" id="basicModal<?php echo $row->id; ?>">
+                                                <div class="modal-dialog  modal-md" role="document">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title">Modification</h5>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            <form action="action_update" method="GET"class="row g-3">
+                                                                <div class="col-md-6">
+                                                                    <label for="inputEmail1"
+                                                                        class="form-label">Designation</label>
+                                                                    <input type="text" class="form-control"
+                                                                        placeholder='designation'name='designation'
+                                                                        value=<?= $row->designation ?>>
+                                                                </div>
+                                                                <div class="col-md-6">
+                                                                    <label for="inputEmail2"
+                                                                        class="form-label">Drapeau</label>
+                                                                    <input type="text" class="form-control"
+                                                                        placeholder='drapeau'name='drapeau'
+                                                                        value=<?= $row->drapeau ?>>
+                                                                </div>
+                                                                <input type="hidden" class="form-control" name='id'
+                                                                    value='<?= $row->id ?>' />
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-danger light"
+                                                                data-bs-dismiss="modal">Close</button>
+                                                            <button type="submit" class="btn btn-primary">Save
+                                                                changes</button>
+                                                        </div>
+                                                        </form>
+                                                    </div>
+                                                    </td>
+                                                    <td>
+                                                        <button type="button" class="btn btn-danger mb-2"
+                                                            data-bs-toggle="modal"
+                                                            data-bs-target="#bbasicModal<?php echo $row->id; ?>">Supprimer</button>
+                                                    </td>
+                                                    <div class="modal fade" id="bbasicModal<?php echo $row->id; ?>">
+                                                        <div class="modal-dialog  modal-md" role="document">
+                                                            <div class="modal-content">
+                                                                <div class="modal-header">
+                                                                    <h5 class="modal-title">Suppression</h5>
+                                                                </div>
+                                                                <div class="modal-body">
+                                                                    <form action="supprimer" method="GET"class="row g-3">
+                                                                        <p style='font-size:28px'>Confirmation de la
+                                                                            suppression!</p> <input type="hidden"
+                                                                            class="form-control" name='id'
+                                                                            value='<?= $row->id ?>' />
+                                                                </div>
+                                                                <div class="modal-footer">
+                                                                    <button type="button" class="btn btn-primary light"
+                                                                        data-bs-dismiss="modal">Close</button>
+                                                                    <button type="submit"
+                                                                        class="btn btn-danger">Supprimer</button>
+                                                                </div>
+                                                                </form>
+                                                            </div>
+                                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 {{ $liste->links() }}
-
             </div>
-
         </div>
     </div>
 @endsection
